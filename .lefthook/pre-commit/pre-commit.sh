ba3e3c0f9b1dd6f058638f2977da7b03dc5230b4
Giang Nguyen <tn.hoanggiang@gmail.com>
2024-05-28 09:57:05 +0700

    [MYAPP-1]: add .lefthook configuration files

--- .lefthook/pre-commit/pre-commit.sh
+++ .lefthook/pre-commit/pre-commit.sh
@@ -1,12 +1,21 @@
 #!/bin/zsh
+# Load env from .env file
+set -o allexport
+source .lefthook/.env
+set +o allexport
+echo "LEFTHOOK_PREFIX: $LEFTHOOK_PREFIX"
+# Get value from environment variable if not throw error
+prefix=$LEFTHOOK_PREFIX
+if [ -z "$prefix" ]; then
+    echo "LEFTHOOK_PREFIX is not set. Please set it in .lefthook/.env file" >&2
+    exit 1
+fi
 echo "Checking your branch name"
+
 LC_ALL=C
-
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
-
-valid_branch_regex='^(feature|bugfix|improvement|release|hotfix)\/[0-9]+_.*$'
-
-message="$local_branch is bad branch name. See example: feature/2_some_text"
+valid_branch_regex='^(feature|bugfix|improvement|release|hotfix)\/'$prefix'-[0-9]+_.*$'
+message="$local_branch is bad branch name. See example: feature/$prefix-2_some_text"
 
 #if [[ $local_branch == "flutter_template" ]]
 #then
